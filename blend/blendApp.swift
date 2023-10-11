eb4557330b51859be8a4e0569159c6dfb94359ee
Dmitry <d.myachn@gmail.com>
2023-10-11 19:16:47 +0300

    Initial Commit

---
+++ blend/blendApp.swift
@@ -0,0 +1,17 @@
+//
+//  blendApp.swift
+//  blend
+//
+//  Created by Dmitiy Myachin on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct blendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
